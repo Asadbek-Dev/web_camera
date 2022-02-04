dd43e53e683bc9dde84d83b3f1158cf9b7cbb322
Asadbek-Dev <asadbek.dev1101@gmail.com>
2022-02-04 17:23:49 +0500

    connected to the webcam

---
+++ index.js
@@ -0,0 +1,10 @@
+let canvas=document.querySelector('#canvas');
+let contect=canvas.getContext("2d");
+let video=document.querySelector('#video');
+
+if(navigator.mediaDevices && navigator.mediaDevices.getUserMedia){
+    navigator.mediaDevices.getUserMedia({video:true}).then((stream)=>{
+        video.srcObject=stream;
+        video.play();
+    });
+}
